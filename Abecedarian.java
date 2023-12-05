b52946e6a258ac7217d9ac7b171e46daa1b69923
Akaiso <akaisobenson@yahoo.com>
2023-12-05 16:54:51 +0100

    find a doubloon was added

--- Abecedarian.java
+++ Abecedarian.java
@@ -1,20 +1,28 @@
+
+//REMEMBER TO MAKE THE USERINPUT AN ARGUMENT IN THE METHOD isAbecedarian
+
+
+
 import java.util.Scanner;
 
 public class Abecedarian{
 
 public static void main(String[] args){
-
 isAbecedarian();
-
 }
 
 public static void isAbecedarian(){
 
-Scanner input = new Scanner(System.in);
-String userInput ;  // FYI: can initialize with an empty string which means it has an index of 0; as you loop through , you add each to the emptyh string
 int index;
+Scanner input = new Scanner(System.in);
+Scanner input2 = new Scanner(System.in);
+String userInput ;  
+String userInput2 ;
+boolean startAfresh = false;
+boolean quitProgram = false;
 System.out.print("THIS PROGRAMS CONFIRMS IF A WORD IS ABECEDARIAN OR NOT\n");
 
+do{
 do{
 System.out.println("Enter a word of your choice:  ");
 if(input.hasNextInt()){
@@ -26,28 +34,53 @@ break;
 }while(input.hasNextInt());
 
 
-System.out.print("out");
 
 userInput = input.nextLine();
 index = userInput.length()-1;
 
-for(int x = 0; x < index; x++){
+
+for(int x = 0; x <= index; x++){
+//System.out.println(x);
 if(x == 0){
-}if(x > 0){
+continue;
+}
+if(x != 0){
 if(userInput.charAt(x) <= userInput.charAt(x-1)){
-System.out.print("not abecedarian");
-if(userInput.charAt(x) > userInput.charAt(x-1)){
+System.out.println("Oopsy! not abecedarian");
+break;
+}if(userInput.charAt(x) > userInput.charAt(x-1)){
 if(x == index){
-System.out.print("An abecedarian");
+System.out.println("Hurray! its abecedarian");
+}
+}
+continue;
+}
+}//end of for loop
+
+
+do{
+System.out.println("NOTE: Quiting the game is still in progress but let's try something");
+System.out.println("To quit enter \"Q\", to continue enter \"Y\" " );
+if(input2.hasNextLine()){
+userInput2 = input2.nextLine().toLowerCase();
+if(userInput2.equals("y")){
+break;
+}else if(userInput2.equals("q")){
+startAfresh = false;
+}else if(!input2.hasNextLine()){
+System.out.println("Either \"Q\" or \"Y\" please: ");
+input2.next();
 
 }
-}
+
 
 }
-}
-}
+}while(input2.hasNextLine());
 
-System.out.print("broken");
+}while(startAfresh = true);
+
+
+
 
 }
 
---
+++ Doubloon.java
@@ -0,0 +1,62 @@
+//Using for loop and charAt to check if each letter appears exactly twice in a word
+//This program takes an input
+//validates the input to be sure its a string
+//checks if the input has a letter appeariing twice in it (doubloon)
+//Tells you if its a doubloon or not
+//YOU CAN TAKE OUT THE VALIDATION TO MAKE IT WORK WITH STRING OF NUBERS
+//Benson E. AkaiSo my first push to stackoverflow Dec 5,2023 . Took me 2 days to get this.
+
+import java.util.Scanner;
+
+public class Doubloon{
+
+public static void main(String[] args){
+isDoubloon();
+}
+
+public static void isDoubloon(){
+
+Scanner input = new Scanner(System.in);
+String userInput ;
+int i;
+int x;
+boolean isNotDoolean = false;
+
+
+System.out.println("Enter a word: ");
+
+//Validate user input
+do{
+if(input.hasNextInt()){
+System.out.println("Numbers are not allowed");
+System.out.println("Try again: ");
+input.nextLine();
+continue;
+}else if(!input.hasNextInt()){
+//userInput = input.nextLine();
+break;
+}
+}while(input.hasNextInt());
+
+//store validated input
+userInput = input.nextLine();
+
+//parse userinput and confirm if it is a doubloon or not
+for(x = 0; x < userInput.length(); x++){
+for( i = 1; i < userInput.length(); i ++){
+if(userInput.charAt(x) == userInput.charAt(i) && x != i){
+System.out.print("Yes! this is a doubloon");
+return;
+}else{isNotDoolean = true;}
+if(x == userInput.length()-1 && isNotDoolean == true){
+System.out.print("Oh no! this is not a doubloon");
+return;
+}
+
+}
+
+} //end of for loop;
+
+
+}
+}
