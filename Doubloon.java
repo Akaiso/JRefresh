9804e236652c8275a390f33b5f8eca1c228c5a14
Akaiso <akaisobenson@yahoo.com>
2023-12-05 16:59:35 +0100

    update on the doubloon

--- Doubloon.java
+++ Doubloon.java
@@ -4,59 +4,60 @@
 //checks if the input has a letter appeariing twice in it (doubloon)
 //Tells you if its a doubloon or not
 //YOU CAN TAKE OUT THE VALIDATION TO MAKE IT WORK WITH STRING OF NUBERS
-//Benson E. AkaiSo my first push to stackoverflow Dec 5,2023 . Took me 2 days to get this.
+//my first push to stackoverflow Dec 5,2023 . Took me 2 days to get this.
 
 import java.util.Scanner;
 
 public class Doubloon{
 
-public static void main(String[] args){
-isDoubloon();
-}
+    public static void main(String[] args){
+        isDoubloon();
+    }
 
-public static void isDoubloon(){
+    public static void isDoubloon(){
 
-Scanner input = new Scanner(System.in);
-String userInput ;
-int i;
-int x;
-boolean isNotDoolean = false;
+        Scanner input = new Scanner(System.in);
+        String userInput ;
+        int i;
+        int x;
+        boolean isNotDoolean = false;
 
 
-System.out.println("Enter a word: ");
+        System.out.println("Enter a word: ");
 
 //Validate user input
-do{
-if(input.hasNextInt()){
-System.out.println("Numbers are not allowed");
-System.out.println("Try again: ");
-input.nextLine();
-continue;
-}else if(!input.hasNextInt()){
+        do{
+            if(input.hasNextInt()){
+                System.out.println("Numbers are not allowed");
+                System.out.println("Try again: ");
+                input.nextLine();
+                continue;
+            }else if(!input.hasNextInt()){
 //userInput = input.nextLine();
-break;
-}
-}while(input.hasNextInt());
+                break;
+            }
+        }while(input.hasNextInt());
 
 //store validated input
-userInput = input.nextLine();
+        userInput = input.nextLine();
 
 //parse userinput and confirm if it is a doubloon or not
-for(x = 0; x < userInput.length(); x++){
-for( i = 1; i < userInput.length(); i ++){
-if(userInput.charAt(x) == userInput.charAt(i) && x != i){
-System.out.print("Yes! this is a doubloon");
-return;
-}else{isNotDoolean = true;}
-if(x == userInput.length()-1 && isNotDoolean == true){
-System.out.print("Oh no! this is not a doubloon");
-return;
+        for(x = 0; x < userInput.length(); x++){
+            for( i = 1; i < userInput.length(); i ++){
+                if(userInput.charAt(x) == userInput.charAt(i) && x != i){
+                    System.out.print("Yes! this is a doubloon");
+                    return;
+                }else{isNotDoolean = true;}
+                if(x == userInput.length()-1 && isNotDoolean == true){
+                    System.out.print("Oh no! this is not a doubloon");
+                    return;
+                }
+
+            }
+
+        } //end of for loop;
+
+
+    }
 }
 
-}
-
-} //end of for loop;
-
-
-}
-}
